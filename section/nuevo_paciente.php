7a41abdd6373f04173f2191b0f3d8823125b2101
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2024-07-09 08:59:57 -0500

    v41

--- section/nuevo_paciente.php
+++ section/nuevo_paciente.php
@@ -5,46 +5,58 @@
     
     <form action="../php/registroPaciente.php" method="POST" id="formulario">
         <div class="grid grid-cols-2 gap-x-4">
-            <h6>Primer Nombre</h6>
-            <h6>Segundo Nombre</h6>
-            <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="primerNombre" id="primerNombre"/>
-            <div id="errorPrimerNombre" style="color: red; font-size: small;"></div>
-            <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="segundoNombre" id="segundoNombre"/>
-            <div id="errorSegundoNombre" style="color: red; font-size: small;"></div>
+            <div>
+                <h6>Primer Nombre</h6>
+                <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="primerNombre" id="primerNombre" style="width: 100%;"/>
+                <div id="errorPrimerNombre" style="color: red; font-size: small;"></div>
+            </div>
+            <div>
+                <h6>Segundo Nombre</h6>
+                <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="segundoNombre" id="segundoNombre" style="width: 100%;"/>
+                <div id="errorSegundoNombre" style="color: red; font-size: small;"></div>
+            </div>
         </div>
         <div class="grid grid-cols-2 gap-x-4">
-            <h6>Apellido Paterno</h6>
-            <h6>Apellido Materno</h6>
-            <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="apellidoPaterno" id="apellidoPaterno"/>            
-            <div id="errorApellidoPaterno" style="color: red; font-size: small;"></div>
-            <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="apellidoMaterno" id="apellidoMaterno"/>
-            <div id="errorApellidoMaterno" style="color: red; font-size: small;"></div>
+            <div>
+                <h6>Apellido Paterno</h6>
+                <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="apellidoPaterno" id="apellidoPaterno" style="width: 100%;"/>            
+                <div id="errorApellidoPaterno" style="color: red; font-size: small;"></div>
+            </div>
+            <div>
+                <h6>Apellido Materno</h6>
+                <input type="text" class="h-8 rounded-lg border-2 border-solid border-blue-600" name="apellidoMaterno" id="apellidoMaterno" style="width: 100%;"/>
+                <div id="errorApellidoMaterno" style="color: red; font-size: small;"></div>
+            </div>
         </div>
         <div class="grid grid-cols-2 gap-x-4">
-            <h6>Grupo Sanguíneo</h6>
-            <h6>Estado Civil</h6>
-            <select class="h-8 rounded-lg border-2 border-solid border-blue-600" name="grupoSanguineo" id="grupoSanguineo">
-                <option value="">Seleccionar...</option>
-                <option value="A+">Grupo A Rh+</option>
-                <option value="A-">Grupo A Rh-</option>
-                <option value="B+">Grupo B Rh+</option>
-                <option value="B-">Grupo B Rh-</option>
-                <option value="AB+">Grupo AB Rh+</option>
-                <option value="AB-">Grupo AB Rh-</option>
-                <option value="O+">Grupo O Rh+</option>
-                <option value="O-">Grupo O Rh-</option>
-            </select>
-            <div id="errorGrupoSanguineo" style="color: red; font-size: small;"></div>
-            <select class="h-8 rounded-lg border-2 border-solid border-blue-600" name="estadoCivil" id="estadoCivil">
-                <option value="">Seleccionar...</option>
-                <option value="soltero">Soltero/a</option>
-                <option value="casado">Casado/a</option>
-                <option value="conviviente">Conviviente</option>
-                <option value="divorciado">Divorciado/a</option>
-                <option value="viudo">Viudo/a</option>
-            </select>
-            <div id="errorEstadoCivil" style="color: red; font-size: small;"></div>
-        </div>
+            <div>
+                <h6>Grupo Sanguíneo</h6>
+                <select class="h-8 rounded-lg border-2 border-solid border-blue-600" name="grupoSanguineo" id="grupoSanguineo" style="width: 100%;">
+                    <option value="">Seleccionar...</option>
+                    <option value="A+">Grupo A Rh+</option>
+                    <option value="A-">Grupo A Rh-</option>
+                    <option value="B+">Grupo B Rh+</option>
+                    <option value="B-">Grupo B Rh-</option>
+                    <option value="AB+">Grupo AB Rh+</option>
+                    <option value="AB-">Grupo AB Rh-</option>
+                    <option value="O+">Grupo O Rh+</option>
+                    <option value="O-">Grupo O Rh-</option>
+                </select>
+                <div id="errorGrupoSanguineo" style="color: red; font-size: small;"></div>
+            </div>
+            <div>
+                <h6>Estado Civil</h6>
+                <select class="h-8 rounded-lg border-2 border-solid border-blue-600" name="estadoCivil" id="estadoCivil" style="width: 100%;">
+                    <option value="">Seleccionar...</option>
+                    <option value="soltero">Soltero/a</option>
+                    <option value="casado">Casado/a</option>
+                    <option value="conviviente">Conviviente</option>
+                    <option value="divorciado">Divorciado/a</option>
+                    <option value="viudo">Viudo/a</option>
+                </select>
+                <div id="errorEstadoCivil" style="color: red; font-size: small;"></div>
+            </div>
+            </div>
         <div class="grid grid-cols-1">
             <h6>Fecha de nacimiento</h6>
             <input type="date" class="form__input h-8 p-4 rounded-lg border-2 border-solid border-blue-600" name="fecha" id="fecha"/>
